9eaaa3bb91495960261e4541153c95bee0030908
djoolee15 <djole.vajagic@gmail.com>
2023-05-23 01:15:10 +0200

    prvi komit za treci domaci

---
+++ ProjekatSBNZ/src/main/java/com/example/ProjekatSBNZ/ProjekatSbnzApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjekatSBNZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatSbnzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatSbnzApplication.class, args);
+	}
+
+}
---
+++ ProjekatSBNZ/src/test/java/com/example/ProjekatSBNZ/ProjekatSbnzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjekatSBNZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatSbnzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
